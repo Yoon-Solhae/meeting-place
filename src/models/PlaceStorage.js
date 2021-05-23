810e9617a5d750ced2b02283a4d90c73c00eab1c
sjun-dev <80457565+sjun-dev@users.noreply.github.com>
2021-05-23 20:48:21 +0900

    Add files via upload

---
+++ src/models/PlaceStorage.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const db = require("../config/db");
+
+class PlaceStorage {
+    static async save(userInfo, userPlace) {
+      return new Promise((resolve, reject) => {
+        const query = "INSERT INTO recommend_data(place_name, univ, gender, addr, lat, lng) VALUES(?, ?, ?, ?, ?, ?);";
+        db.query(query, [userPlace.name, userInfo.univ, userInfo.gender, userPlace.addr.address_name, userPlace.lat, userPlace.lng], (err) => {
+          if (err) reject(`${err}`);
+          else resolve({ success: true });
+        });
+      });
+    }
+
+    static async getRecommendData(univ, gender) {
+      return new Promise((resolve, reject) => {
+        const query = `SELECT COUNT(place_name), place_name, addr, lat, lng FROM recommend_data
+                        WHERE univ = ? and gender = ?
+                        GROUP BY place_name, addr
+                        ORDER BY COUNT(place_name) DESC;`;
+        db.query(query, [univ, gender], (err, data) => {
+          if (err) reject(`${err}`);
+          else {
+            resolve(data);
+          }
+        });
+      });
+    }
+  }
+
+module.exports = PlaceStorage;
